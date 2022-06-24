195ca6626cc1b17f176888090cb97d344a884f1a
hadiuzzaman143 <107674015+hadiuzzaman143@users.noreply.github.com>
2022-06-24 19:53:26 +0530

    Update name.java

--- name.java
+++ name.java
@@ -2,6 +2,7 @@ class name
 {
     public static void main(String args[])
     {
+        //output the value
         System. out. println("Nane: Hadiuzzaman");
         System. out. println("Dpt: CSE");
         System. out. println("Course: B.Tech");
