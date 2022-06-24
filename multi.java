e6653df67cd0c50d09f9d9bbcb11eb1b8cf55226
hadiuzzaman143 <107674015+hadiuzzaman143@users.noreply.github.com>
2022-06-24 19:44:20 +0530

    Update multi.java

--- multi.java
+++ multi.java
@@ -2,7 +2,8 @@ class multiplication
 {  
     public static void main(String args[])   
   {  
-    int a = 10, b = 5,   
+    int a = 10, b = 5, 
+    //Multiplying two numbers 
     multiplication  = a * b;  
     System.out.println("The  multiplication of numbers is: "+multiplication );  
    }  
