7a46c53ac5859bdd27aa22db15e9e67f79ef6a13
hadiuzzaman143 <107674015+hadiuzzaman143@users.noreply.github.com>
2022-06-24 19:30:23 +0530

    Update division.java

--- division.java
+++ division.java
@@ -2,8 +2,8 @@ class  division
 {  
     public static void main(String args[])   
   {  
-    int a = 50, b = 2,   
-     division  = a / b;  
+     int a = 50, b = 2,   
+     division  = a / b; //division two numbers 
     System.out.println("The of division numbers is: "+division);  
    }  
 }  
