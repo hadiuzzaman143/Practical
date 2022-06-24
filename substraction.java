d18379d6434c83b2c0d85c56e033bed80fa773f4
hadiuzzaman143 <107674015+hadiuzzaman143@users.noreply.github.com>
2022-06-24 19:58:00 +0530

    Update substraction.java

--- substraction.java
+++ substraction.java
@@ -2,7 +2,8 @@ class substraction
 {  
     public static void main(String args[])   
   {  
-    int a = 10, b = 5,   
+    int a = 10, b = 5,
+    //Performing the subtraction of the two numbers
     substraction = a - b;  
     System.out.println("The substraction of numbers is: "+substraction);  
    }  
