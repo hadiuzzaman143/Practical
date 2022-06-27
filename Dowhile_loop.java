bcf824ea8cfb8fbcd8eddeb9fae0972ef20af447
hadiuzzaman143 <107674015+hadiuzzaman143@users.noreply.github.com>
2022-06-27 12:45:23 +0530

    Add files via upload

---
+++ Dowhile_loop.java
@@ -0,0 +1,14 @@
+public class Dowhile_loop 
+{
+    public static void main(String[]args)
+    {
+       int i = 0;
+       System.out.println("First 10 enen number \n");
+       do{
+        System.out.println(i);
+        i = i + 2;
+        }
+        while(i<=10);
+       
+      }  
+}
---
+++ For_loop.java
@@ -0,0 +1,12 @@
+public class For_loop 
+{
+  public static void main(String[]args)
+  {
+    int i = 0;
+    for(int j = 1; j<=10; j++)
+    {
+        i = i + j;
+    }
+    System.out.println("The nam of first 10 numbers is:-"+ i);
+  }  
+}
