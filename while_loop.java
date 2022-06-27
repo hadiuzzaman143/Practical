0081e57d2da356653d6373bfd18746eaba8f71c1
hadiuzzaman143 <107674015+hadiuzzaman143@users.noreply.github.com>
2022-06-27 12:47:36 +0530

    Add files via upload

---
+++ while_loop.java
@@ -0,0 +1,13 @@
+public class while_loop
+{
+   public static void main(String[]args)
+{
+   int i = 0;
+   System.out.println("First 10 enen number \n");
+   while(i<=10)
+   {
+   System.out.println(i);
+   i = i +2;
+   }
+  }
+}
