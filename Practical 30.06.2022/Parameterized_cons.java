39aa4928ffa746fb0b525913f4d035f870bf211a
hadiuzzaman143 <107674015+hadiuzzaman143@users.noreply.github.com>
2022-06-30 21:22:53 +0530

    Update Parameterized_cons.java

--- Practical 30.06.2022/Parameterized_cons.java
+++ Practical 30.06.2022/Parameterized_cons.java
@@ -9,9 +9,9 @@ public class Parameterized_cons
 	}
 	public static void main(String[] args) 
     {   //  main method
-		Parameterized_cons obj = new Parameterized_cons("Java");    // object 1
+		Parameterized_cons obj = new Parameterized_cons("C");    // object 1
 		Parameterized_cons obj1 = new Parameterized_cons("C++");    // object 2
-		Parameterized_cons obj2 = new Parameterized_cons("C");      // object 3
+		Parameterized_cons obj2 = new Parameterized_cons("JAVA");      // object 3
 		System.out.println("This name is " + obj.name);
 		System.out.println("This name is " + obj1.name);
 		System.out.println("This name is " + obj2.name);
