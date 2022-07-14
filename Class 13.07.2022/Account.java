d2a769b5fd18a4222816bf0c8ea3455a2566df39
hadiuzzaman143 <107674015+hadiuzzaman143@users.noreply.github.com>
2022-07-14 10:44:14 +0530

    Add files via upload

---
+++ Class 13.07.2022/Account.java
@@ -0,0 +1,55 @@
+class Account 
+{ 
+    private long acc_no; 
+    private String name,email; 
+    private float amount; 
+    //public getter and setter methods 
+    public long getAcc_no() 
+    { 
+        return acc_no; 
+    } 
+    public void setAcc_no(long acc_no) 
+    { 
+        this.acc_no = acc_no; 
+    } 
+    public String getName() 
+    { 
+        return name; 
+    } 
+    public void setName(String name) 
+    { 
+        this.name = name; 
+    } 
+    public String getEmail() 
+    { 
+        return email; 
+    } 
+    public void setEmail(String email) 
+    { 
+        this.email = email; 
+    } 
+    public float getAmount() 
+    { 
+        return amount; 
+    } 
+    public void setAmount(float amount) 
+    { 
+        this.amount = amount; 
+    } 
+       
+    } 
+    class Account2
+    { 
+    public static void main(String[] args) 
+    { 
+         
+        Account acc=new Account(); 
+        
+        acc.setAcc_no(7310805450L); 
+        acc.setName("Hadiuz"); 
+        acc.setEmail("Hadiuz@gmail.com"); 
+        acc.setAmount(100000); 
+         
+        System.out.println(acc.getAcc_no()+" "+acc.getName()+" "+acc.getEmail()+" "+acc.getAmount()); 
+    } 
+    }
