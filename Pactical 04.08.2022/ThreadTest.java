b46d8cb7d2dfd8901dcfe9b8196950ca436588f2
hadiuzzaman143 <107674015+hadiuzzaman143@users.noreply.github.com>
2022-08-05 21:42:56 +0530

    Update ThreadTest.java

--- Pactical 04.08.2022/ThreadTest.java
+++ Pactical 04.08.2022/ThreadTest.java
@@ -1,10 +1,16 @@
 
-public class ThreadTest extends Thread{   // thread
-	public void run() {    // run() method
-		System.out.println("i am a thread");
+public class ThreadTest extends Thread
+{   
+// thread
+	public void run() 
+	{    
+	// run() method
+	  System.out.println("i am a thread");
 	}
-	public static void main(String[] args) {   // main method
-		ThreadTest t1 = new ThreadTest();    // object
-		t1.start();   // to invoke the run() method
+	 public static void main(String[] args) 
+	 {   
+	     // main method
+	  ThreadTest t1 = new ThreadTest();    // object
+	  t1.start();   // to invoke the run() method
 	}
 }
